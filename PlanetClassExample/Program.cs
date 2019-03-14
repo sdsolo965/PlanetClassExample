935ae6220e1967a548d2c3e8415fad69c9bcd6f9
Shane Solomon <shsolomo@microsoft.com>
2019-03-14 12:56:36 -0700

    Add project files.

---
+++ PlanetClassExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpaceShip ship1 = new SpaceShip("Enterprise", "Flacon Heavy");
+
+            SpaceShip ship2 = new SpaceShip("Apollo", "Falcon Light");
+
+            List<SpaceShip> shipList = new List<SpaceShip>();
+
+            shipList.Add(ship1);
+            shipList.Add(ship2);
+
+            foreach (var ship in shipList)
+            {
+                Console.WriteLine($"Ship name is: {ship.Name}");
+                Console.WriteLine($"Ship type is: {ship.Type}");
+
+            }
+
+            ship1.GetShipName();
+
+            ship2.GetShipName();
+        }
+    }
+}
---
+++ PlanetClassExample/SpaceShip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetClassExample
+{
+    class SpaceShip
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public SpaceShip(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public void GetShipName()
+        {
+            Console.WriteLine($"Ship name is: {Name}");
+        }
+    }
+}
